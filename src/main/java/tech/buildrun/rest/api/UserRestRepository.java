9f7e4c89737102def069784c1a16bc5b73b80005
Eryck Santos <dev.erycksantos@gmail.com>
2025-06-08 13:17:18 -0300

    Adicionando filtragem na API.

--- src/main/java/tech/buildrun/rest/api/UserRestRepository.java
+++ src/main/java/tech/buildrun/rest/api/UserRestRepository.java
@@ -2,9 +2,11 @@ package tech.buildrun.rest.api;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.buildrun.rest.entity.UserEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(
@@ -14,4 +16,6 @@ import java.util.UUID;
 public interface UserRestRepository extends
         CrudRepository<UserEntity, UUID>,
         PagingAndSortingRepository<UserEntity, UUID> {
-}
+
+    List<UserEntity> findByName(@Param("name") String name);
+ }
